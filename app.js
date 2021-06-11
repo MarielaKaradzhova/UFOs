133d9ae44a706c1267007a2dda480c4a31b991c4
Mariela Karadzhova <76116569+MarielaKaradzhova@users.noreply.github.com>
2021-06-11 18:02:50 -0400

    adding html and java final working code

--- app.js
+++ app.js
@@ -24,21 +24,26 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
+var allFilters={}
 
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-
+    var changeFilter=d3.select(this)
     // 4b. Save the value that was changed as a variable.
-
+    var changeValue=changeFilter.property("value")
     // 4c. Save the id of the filter that was changed as a variable.
-
+    var changeID=changeFilter.attr("id")
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
+    if (changeValue){
+      allFilters[changeID]=changeValue
+    } else {
+      delete allFilters[changeID]
+    }
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
@@ -49,18 +54,20 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    var filterData = tableData
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(allFilters).forEach(([key,value])=>{
+      filterData=filterData.filter(row=>row[key]==value)
+    })
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filterData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("input").on("change", updateFilters)
   
   // Build the table when the page loads
   buildTable(tableData);
